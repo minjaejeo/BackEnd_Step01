1227f43608eae5b478477e97b416965ebe587fb3
NCP08 <navercloudplatform08.gmail.com>
2024-03-27 14:00:00 +0900

    3.27 공부 내용

---
+++ _01_FirstWebProject/_02_GetRequest/src/main/java/_02_GetRequest/SubOperator.java
@@ -0,0 +1,16 @@
+package _02_GetRequest;
+
+public class SubOperator implements Operator {
+
+	@Override
+	public String getName() {
+		
+		return "-";
+	}
+
+	@Override
+	public double execute(double a, double b) throws Exception {
+		return a - b;
+	}
+
+}
