e3e8e375b7ec2af8012a61def6c93dc952ede4f0
minjaejeo <157557014+minjaejeo@users.noreply.github.com>
2024-03-31 21:22:48 +0900

    _18_Question 까지 복습

--- _18_Question/src/main/java/spms/servlets/MemberDeleteServlet.java
+++ _18_Question/src/main/java/spms/servlets/MemberDeleteServlet.java
@@ -1,11 +1,8 @@
 package spms.servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.Statement;
 
 import javax.servlet.ServletContext;
@@ -18,36 +15,32 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/member/delete")
 @SuppressWarnings("serial")
 public class MemberDeleteServlet extends HttpServlet {
-
+	
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.setContentType("text/html;charset=UTF-8");
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Connection conn = null;
-		PreparedStatement stmt = null;
+		Statement stmt = null;
 		try {
 			ServletContext sc = this.getServletContext();
 			Class.forName(sc.getInitParameter("driver"));
 			conn = DriverManager.getConnection(
-						sc.getInitParameter("url"),
-						sc.getInitParameter("username"),
-						sc.getInitParameter("password")
+					sc.getInitParameter("url"),
+					sc.getInitParameter("username"),
+					sc.getInitParameter("password")
 					);
-			stmt = conn.prepareStatement("DELETE FROM members WHERE mno=?");
-			stmt.setInt(1, Integer.parseInt(req.getParameter("no")));
-			stmt.executeUpdate();
-			
-
-			resp.sendRedirect("list");
-			
-			
+			stmt = conn.createStatement();
+			stmt.executeUpdate(
+					"DELETE FROM members WHERE mno=" + 
+					request.getParameter("no")
+					);
+			response.sendRedirect("list");
 		}catch(Exception e) {
 			throw new ServletException(e);
 		}finally {
-			
-			try {if(stmt!=null) stmt.close();} catch(Exception e) {}
-			try {if(conn!=null) conn.close();} catch(Exception e) {}			
+			try {if (stmt != null) stmt.close();} catch(Exception e) {}
+			try {if (conn != null) conn.close();} catch(Exception e) {}
 		}
-		
-}
+	}
+	
 }
 
