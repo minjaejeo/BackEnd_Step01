b13b9f80d79995ed4b5c8a1a2c66dd6291d76f63
NCP08 <navercloudplatform08.gmail.com>
2024-04-01 16:53:53 +0900

    _43_Tomcat_DataSource까지 수업 내용

--- _43_Tomcat_DataSource/src/main/java/spms/servlets/MemberAddServlet.java
+++ _43_Tomcat_DataSource/src/main/java/spms/servlets/MemberAddServlet.java
@@ -3,7 +3,6 @@ package spms.servlets;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
@@ -12,26 +11,29 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import spms.dao.MemberDao;
 import spms.vo.Member;
 
+@WebServlet("/member/add")
 @SuppressWarnings("serial")
-@WebServlet("/auth/login")
-public class LogInServlet extends HttpServlet{
-
+public class MemberAddServlet extends HttpServlet{
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		RequestDispatcher rd = req.getRequestDispatcher("/auth/LogInForm.jsp");
+		System.out.println("MemberAddServlet::doGet() 호출");
+		
+		RequestDispatcher rd = req.getRequestDispatcher(
+				"/member/MemberForm.jsp");
 		rd.forward(req, resp);
 	}
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("MemberAddServlet::doPost() 호출");
+		
 //		Connection conn = null;
 //		PreparedStatement stmt = null;
-//		ResultSet rs = null;
 		
 		try {
 			ServletContext sc = this.getServletContext();
@@ -42,36 +44,48 @@ public class LogInServlet extends HttpServlet{
 
 		      MemberDao memberDao = new MemberDao();
 		      memberDao.setConnection(conn);
-		      */
-		      
-		      Member member = memberDao.exist(
-		    		  req.getParameter("email"), 
-		    		  req.getParameter("password"));
-		      if (member != null) {
-		        HttpSession session = req.getSession();
-		        session.setAttribute("member", member);
-		        resp.sendRedirect("../member/list");
+		    */
+		      memberDao.insert(new Member()
+		        .setEmail(req.getParameter("email"))
+		        .setPassword(req.getParameter("password"))
+		        .setName(req.getParameter("name")));
 
-		      } else {
-		        RequestDispatcher rd = req.getRequestDispatcher(
-		            "/auth/LogInFail.jsp");
-		        rd.forward(req, resp);
-		      }			
-			
+			resp.sendRedirect("list");
+
 		}catch(Exception e) {
 			e.printStackTrace();
 			req.setAttribute("error", e);
 			RequestDispatcher rd = req.getRequestDispatcher("/Error.jsp");
 			rd.forward(req, resp);
+			
 		}
 		/*
 		finally {
-			try {if(rs!=null) rs.close();} catch(Exception e) {}
 			try {if(stmt!=null) stmt.close();} catch(Exception e) {}
+			
+			// Connection객체를 1개 생성해서 ServletContext영역에 보관하고
+			// 모든 Servlet이 공유해서 사용하므로 이곳에서 닫으면 안된다.
+			//try {if(conn!=null) conn.close();} catch(Exception e) {}
 		}
 		*/
 	}
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
