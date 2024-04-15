3156b4cb01df98d7f814d3ecc0823cf917260a9b
NCP08 <navercloudplatform08.gmail.com>
2024-04-15 18:16:00 +0900

    Oracle_Question_36, _38까지 복습

--- Oracle_sql/question/_36_SELECT_Question.sql
+++ Oracle_sql/question/_36_SELECT_Question.sql
@@ -1,44 +1,71 @@
 
 1) 화학과 학생과 평점이 동일한 학생들을 검색하세요
-SELECT avr
- FROM student
- WHERE major='화학';
 
-SELECT *
+-- SELECT avr
+--  FROM student
+--  WHERE major='화학';
+
+-- SELECT *
+--  FROM student
+--  WHERE avr IN (SELECT avr
+--                 FROM student
+--                 WHERE major='화학')
+--     AND major!='화학';
+
+SELECT sno, sname, major, avr
  FROM student
  WHERE avr IN (SELECT avr
                 FROM student
                 WHERE major='화학')
-    AND major!='화학';
-2) 화학과 교수와 부임일이 같은 직원을 검색하세요
-SELECT hiredate
-    FROM professor
-    WHERE section='화학';
+    AND major!='화학'
+    ORDER BY major;
 
-SELECT *
- FROM professor
+2) 화학과 교수와 부임일이 같은 직원을 검색하세요
+-- SELECT hiredate
+--     FROM professor
+--     WHERE section='화학';
+
+-- SELECT *
+--  FROM professor
+--  WHERE hiredate IN (SELECT hiredate
+--                     FROM professor
+--                     WHERE section='화학')
+--     AND section!='화학';
+
+SELECT eno, ename, hiredate
+ FROM emp
  WHERE hiredate IN (SELECT hiredate
                     FROM professor
-                    WHERE section='화학')
-    AND section!='화학';
+                    WHERE section='화학');
+
+
 
 3) 화학과 학생과 같은 학년에서 평점이 동일한 학생들을 검색하세요
+-- SELECT avr
+--  FROM student
+--  WHERE major='화학';
 
-SELECT avr
- FROM student
- WHERE major='화학';
+-- SELECT DISTINCT syear
+--  FROM student
+--  WHERE major='화학';
 
-SELECT DISTINCT syear
- FROM student
- WHERE major='화학';
+-- SELECT *
+--  FROM student
+--  WHERE avr IN (SELECT avr
+--                 FROM student
+--                 WHERE major='화학')
+--     AND syear IN (SELECT DISTINCT syear
+--                     FROM student
+--                     WHERE major='화학');
 
-SELECT *
+SELECT sno, sname, major, syear, avr
  FROM student
- WHERE avr IN (SELECT avr
-                FROM student
-                WHERE major='화학')
-    AND syear IN (SELECT DISTINCT syear
-                    FROM student
-                    WHERE major='화학');
+ WHERE (syear, avr) IN (SELECT syear, avr
+                        FROM student
+                        WHERE major='화학')
+    AND major!='화학'
+    ORDER BY major;
+
+
 
 
--- Oracle_sql/question/_38_DML_Question.sql
+++ Oracle_sql/question/_38_DML_Question.sql
@@ -1,11 +1,23 @@
 
 --1) 모든 학생의 성적을 4.5만점 기준으로 수정하세요
-UPDATE student SET avr=avr*4.5/4.0;
-SELECT * FROM student;
+-- UPDATE student SET avr=avr*4.5/4.0;
+-- SELECT * FROM student;
+
+UPDATE student SET
+ avr=avr*4.5/4.0;
+
+ROLLBACK;
 
 --2) 모든 교수의 부임일자를 100일 앞으로 수정하세요
-UPDATE professor SET hiredate=hiredate-100;
-SELECT * FROM professor;
+-- UPDATE professor SET hiredate=hiredate-100;
+-- SELECT * FROM professor;
+
+SELECT pno, pname, hiredate FROM professor;
+
+UPDATE professor SET 
+ hiredate = hiredate - 100;
+
+ROLLBACK;
 
 --3) 화학과 2학년 학생의 정보를 삭제하세요
 DELETE FROM student
